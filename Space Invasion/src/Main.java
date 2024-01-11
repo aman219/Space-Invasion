b607968f4764ebe63c630989ee2d4c21e82cf3fc
aman219 <65351629+aman219@users.noreply.github.com>
2024-01-11 15:23:59 +0530

    Project Created

---
+++ Space Invasion/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(1280, 650);
+		frame.setTitle("Game Window");
+		System.out.println("Working...");
+	}
+}
