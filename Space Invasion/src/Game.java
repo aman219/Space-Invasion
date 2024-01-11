d28dc9534c8127fc040d1e7c7b7307e76deaeafe
manish071077 <iprincesingh7272@gmail.com>
2024-01-11 16:19:51 +0530

    Displaying playes name

--- Space Invasion/src/Game.java
+++ Space Invasion/src/Game.java
@@ -1,7 +1,9 @@
 
 public class Game {
+	Player p1=new Player("Aman");
+	Player p2=new Player("Manish");
 	void display()
 	{
-		System.out.print("This is game class.");
+		System.out.println("This is game between: "+ p1.getPlayer() + " & "+ p2.getPlayer());
 	}
 }
